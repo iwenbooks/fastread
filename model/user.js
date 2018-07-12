12ae8e43e483481ca77c191478db773f04b2522b
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-07-12 15:59:51 +0800

    Update user.js

--- model/user.js
+++ model/user.js
@@ -9,7 +9,9 @@ const userSchema = new Schema({
     "password": { type: String },
     "books": [{
         book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
-        segment: { type: mongoose.Schema.Types.ObjectId, ref: 'Segment' }
+        segment: { type: mongoose.Schema.Types.ObjectId, ref: 'Segment' },
+        whetherOrNotToRead:{type:Boolean,default:false}
+
     }],
     "records": [{
         book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
