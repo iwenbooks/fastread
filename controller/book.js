42ef306cf27b785d983d417cb5a184241bb57c3e
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-07-12 20:58:14 +0800

    Update book.js

--- controller/book.js
+++ controller/book.js
@@ -34,14 +34,7 @@ const getInfoById = async (ctx) => {
             path: 'segments',
             select: {
                 "_id":1,
-                "content:":0,
-                "name":0,
-                "level":0,
-                "words":0,
-                "created":0,
-                "updated":0,
-                "comments":0,
-                "commentNum":1
+                "content:":0
             }
         })
         .exec()
