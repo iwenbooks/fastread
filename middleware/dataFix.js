7de25fd9b4673ef071b43608530a09ce8f86d355
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-07-24 20:41:54 +0800

    xxx

--- middleware/dataFix.js
+++ middleware/dataFix.js
@@ -3,12 +3,11 @@ const BookModel = require("../model/book");
 const addImdb =async (ctx)=>{
     const average_ratingVal = 3.64278616819345;
     const m = 10000;
-    let booknum = await BookModel.find().count();
-    for(let i=0;i<booknum;i+=5){
+    for(let i=0;i<16783;i+=10){
         let book = await BookModel.find().skip(i).limit(5);
-        for(let j=0;j<5;j++){
-            let IMDB = book[i].goodreads_ratingVal*(book[i].goodreads_ratings/(book[i].goodreads_ratings+m))+(m/(book[i].goodreads_ratings+m))*average_ratingVal;
-            await BookModel.update({"_id":book[i]._id},{$set:{"IMDB":IMDB}});
+        for(let j=0;j<10;j++){
+            let IMDB = book[j].goodreads_ratingVal*(book[j].goodreads_ratings/(book[j].goodreads_ratings+m))+(m/(book[j].goodreads_ratings+m))*average_ratingVal;
+            await BookModel.update({"_id":book[j]._id},{$set:{"IMDB":IMDB}});
         }
     }
 };
