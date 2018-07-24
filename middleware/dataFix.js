490846ac59b29fbeb70b023348ade4db5752f5cf
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-07-24 20:46:23 +0800

    xxx

--- middleware/dataFix.js
+++ middleware/dataFix.js
@@ -5,6 +5,7 @@ const addImdb =async (ctx)=>{
     const m = 10000;
     for(let i=0;i<16783;i+=10){
         let book = await BookModel.find().skip(i).limit(5);
+        console.log(i);
         for(let j=0;j<10;j++){
             let IMDB = book[j].goodreads_ratingVal*(book[j].goodreads_ratings/(book[j].goodreads_ratings+m))+(m/(book[j].goodreads_ratings+m))*average_ratingVal;
             await BookModel.update({"_id":book[j]._id},{$set:{"IMDB":IMDB}});
