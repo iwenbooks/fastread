8bde3e79b3e10c5e74aa5d571faa59bd4ea4ee10
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-07-25 16:45:10 +0800

    xxx

--- controller/user.js
+++ controller/user.js
@@ -596,7 +596,7 @@ const uploadAvatar = async ctx => {
     let userId = token.id;
     let path = config.avatar_path + userId + '.jpg';
     console.log(path);
-    let file = ctx.request.body;
+    let file = ctx.request.file;
     console.log(file);
     await fs.writeFile(path,file,function (err) {
         if(err){
