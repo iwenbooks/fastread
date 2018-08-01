945203455bf4d9a9d3604d00b80aa53bef0e2198
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2018-08-02 01:34:43 +0800

    xxx

--- controller/book.js
+++ controller/book.js
@@ -261,7 +261,7 @@ const searchByFirstAlphabet=async(ctx)=>{
     let book = await BookModel.find({"author":{$regex:re,$options:"i"}},{"author":1}).sort({"IMDB":-1,"likeNum":-1,"cover":-1,"commentary":-1}).skip(skip).limit(limit).exec();
     let result = [];
     for(let i in book){
-        result.push(i["author"])
+        result.push(i.author)
     }
     ctx.body = result;
     ctx.status = 200;
